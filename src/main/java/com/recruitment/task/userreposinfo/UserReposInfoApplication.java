02bb32b1e8b4d4213cf0748dc279a70b4f92dfa3
Michał Polewaczyk <s22126@pjwstk.edu.pl>
2023-09-10 20:26:00 +0200

    Initialized project

---
+++ src/main/java/com/recruitment/task/userreposinfo/UserReposInfoApplication.java
@@ -0,0 +1,13 @@
+package com.recruitment.task.userreposinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserReposInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserReposInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/recruitment/task/userreposinfo/UserReposInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recruitment.task.userreposinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserReposInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
